b92fe03ba3797ec1d112a58e6d8af896286c607b
Purple-Code-sh <purple.code.sh@gmail.com>
2025-02-07 12:33:58 -0600

    Chore: updates favicon

--- src/ui/booking/Form.js
+++ src/ui/booking/Form.js
@@ -99,13 +99,14 @@ export class RegisterForm extends LitElement {
 
     submitBtn.disabled = true;
     submitBtn.innerHTML = `
-      <div class="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white" role="status">
-        <span class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-          Loading...
-        </span>
-      </div>
+    <div class="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white" role="status">
+    <span class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+    Loading...
+    </span>
+    </div>
     `;
 
+    // return;
     const formData = new FormData(form);
 
     try {
